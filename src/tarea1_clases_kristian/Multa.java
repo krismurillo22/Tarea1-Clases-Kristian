57ab3f960af1ea155e1c7afc1ea4bcf07b4a48f3
krismurillo22 <kristian_murillo@unitec.edu>
2024-07-20 21:50:55 -0600

    Creacion de clase multa, con todos sus atributos, constructor y funciones

--- src/tarea1_clases_kristian/Multa.java
+++ src/tarea1_clases_kristian/Multa.java
@@ -9,6 +9,59 @@ package tarea1_clases_kristian;
  * @author User
  */
 public class Multa {
-
     
+    private int codigo;
+    private String nombreinfractor;
+    private String tipo;
+    private double montoPagar;
+    private boolean pagada;
+    
+    public static double TIPO_GRAVE = 1000.0;
+    public static double TIPO_MEDIO = 500.0;
+    public static double TIPO_LEVE = 100.0;
+    
+    public Multa(int codigo, String nombre, String tipo){
+        this.codigo=codigo;
+        nombreinfractor=nombre;
+        this.tipo=tipo;
+        pagada=false;
+        
+        if (this.tipo.equals("LEVE")){
+            this.montoPagar=TIPO_LEVE;
+        }else if (this.tipo.equals("MEDIO")){
+            this.montoPagar=TIPO_MEDIO;
+        }else if (this.tipo.equals("GRAVE")){
+            this.montoPagar=TIPO_GRAVE;
+        }else {
+            this.tipo = "LEVE";
+            this.montoPagar=TIPO_LEVE;
+        }
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    public double getMontoPagar() {
+        return montoPagar;
+    }
+    
+    public boolean isPagada() {
+        return pagada;
+    }
+    
+    public String print(){
+        String estado = pagada ? "Pagada por un monto de: Lps. "+montoPagar:"Multa pendiente";
+        String print ="Código: "+this.codigo+
+                    "\nNombre del Infractor: "+this.nombreinfractor+
+                    "\nTipo de Multa: "+this.tipo+
+                    "\nMonto a Pagar: "+this.montoPagar+
+                    "\n"+estado;
+        return print;
+    }
+    
+    public String pay() {
+        this.pagada = true;
+        return "Multa con Código "+codigo+" fue pagada por un monto de Lps. "+montoPagar;
+    }
 }
