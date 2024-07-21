f184c77c406783435162046531e994cbd285e031
krismurillo22 <kristian_murillo@unitec.edu>
2024-07-20 21:16:51 -0600

    Subida de projecto a repositorio

---
+++ src/tarea1_clases_kristian/Multa.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea1_clases_kristian;
+
+/**
+ *
+ * @author User
+ */
+public class Multa {
+
+    
+}
